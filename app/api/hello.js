1fa24a737655dadc3ca419e67405aae2aa7e3afb
Ralph Landon <ralph7c2@gmail.com>
2021-08-20 12:24:27 -0700

    Throw error on NaN

--- app/api/hello.js
+++ app/api/hello.js
@@ -1,20 +1,15 @@
 const handler = async (req, res) => {
   res.setHeader("Content-Type", "application/json")
-  try {
-    const timeout = parseInt(req.query.timeout)
-    if (isNaN(timeout)) {
-      handleError(res, "not a number")
-      return
-    }
-    console.log(timeout)
-    await sleep(timeout * 1000)
-    console.log("done sleeping")
-    res.statusCode = 200
-    res.end(JSON.stringify({ name: "John Doe", timeout }))
-  } catch (e) {
-    handleError(res, e)
+  const timeout = parseInt(req.query.timeout)
+  if (isNaN(timeout)) {
+    throw "error"
     return
   }
+  console.log(timeout)
+  await sleep(timeout * 1000)
+  console.log("done sleeping")
+  res.statusCode = 200
+  res.end(JSON.stringify({ name: "John Doe", timeout }))
 }
 
 const handleError = (res, e) => {
